e7b902eba19a8180b0170cc9b176897317afee99
Will Sinclair <will@willsinclair.ca>
2017-06-02 17:03:51 -0400

    added soft minimize/close buttons

--- app/assets/js/coin-monitor.js
+++ app/assets/js/coin-monitor.js
@@ -4,20 +4,92 @@
 
 const request = require('request');
 const $ = require('jquery');
+const fs = require('fs');
+const os = require('os');
 
 class CoinMonitor {
-	constructor(settings) {
-		this.settings = settings;
+	constructor() {
+		this.settingsDir = os.homedir() + "/.bitcoin-monitor";
+		this.settingsPath = this.settingsDir + "/config.json";
+		this.refreshSettings();
+		this.getSpotPrice();
+
+		setInterval(() => {
+			this.getSpotPrice();
+		}, this.settings.refreshrate);
 	}
 
-	getSpotPrice(callback) {
+	getSpotPrice() {
+		var base = this.settings.baseCurrency.toLowerCase();
+		var target = this.settings.targetCurrency.toLowerCase();
 		request({
-				url: "https://api.cryptonator.com/api/ticker/btc-cad",
+				url: "https://api.cryptonator.com/api/ticker/" + base + "-" + target
 			},
 			(error, response, body) => {
-				if (error) return callback(new Error('Spot price request failed'));
-				return callback(null, body);
+				if (error) console.log('oops');
+				var data = JSON.parse(body).ticker;
+				console.log(data);
+				data.price = Math.round(data.price * 100) / 100;
+				$("#current-price-display").text("$" + data.price + " " + data.target);
 			});
+	}
+
+	refreshSettings() {
+		// create the app's home folder if it doesn't exist
+		if (!fs.existsSync(this.settingsDir)) {
+			fs.mkdirSync(this.settingsDir);
+		}
+		// if the config file exists
+		if (fs.existsSync(this.settingsPath)) {
+			// read it into the object
+			this.settings = JSON.parse(fs.readFileSync(this.settingsPath, 'utf8'));
+		}
+		// if no config file exists
+		else {
+			// write one with default settings
+			var defaultSettings = {
+				baseCurrency: "BTC",
+				targetCurrency: "CAD",
+				refreshrate: "3000"
+			};
+			this.settings = defaultSettings;
+			fs.writeFile(this.settingsPath, JSON.stringify(defaultSettings), {
+				encoding: 'utf8'
+			});
+		}
+		this.setUiFromSettings();
+	}
+
+	updateSetting(name, value) {
+		try {
+			var tempSettings = this.settings;
+			tempSettings.name = value;
+			this.settings = tempSettings;
+			this.changeUiFromSetting(name, value);
+			fs.writeFile(this.settingsPath, JSON.stringify(tempSettings), {
+				encoding: 'utf8'
+			});
+		} catch (Error) {
+			console.log('Failed to set setting' + name + ' to ' + value);
+		}
+	}
+
+	changeUiFromSetting(name, value) {
+		switch (name) {
+			case 'baseCurrency':
+				$("#base-currency-display").text(value);
+				break;
+			case 'targetCurrency':
+				$("#target-currency-display").text(value);
+				break;
+		}
+	}
+
+	setUiFromSettings() {
+		// base currency
+		$("#base-currency-display").text(this.settings.baseCurrency);
+		// target currency
+		$("#target-currency-display").text(this.settings.targetCurrency);
 	}
 }
 
--- app/assets/js/renderer.js
+++ app/assets/js/renderer.js
@@ -1,18 +1,24 @@
 const $ = require('jquery');
 const remote = require('electron').remote;
-const win = remote.getCurrentWindow();
 const CoinMonitor = require('./coin-monitor');
 const bootstrap = require('bootstrap');
-var settings = remote.getGlobal('settings');
-var monitor = new CoinMonitor(settings);
+var monitor = new CoinMonitor();
 
-
-$("#spot-price-update-button").on('click', function () {
-	monitor.getSpotPrice((err, res) => {
-		if (err) return console.log(err);
-		var data = JSON.parse(res);
-		console.log(data.ticker);
-		$("#current-price-display").text("$" + data.data.amount + " " + data.data.currency);
-	});
-	return false;
-});
+(function () {
+	function init() {
+		// minimize and close buttons
+		document.getElementById('minimize-button').addEventListener('click', (e) => {
+			var window = remote.getCurrentWindow();
+			window.minimize();
+		});
+		document.getElementById('close-button').addEventListener('click', (e) => {
+			var window = remote.getCurrentWindow();
+			window.close();
+		});
+	}
+	document.onreadystatechange = () => {
+		if (document.readyState == 'complete') {
+			init();
+		}
+	}
+})();
