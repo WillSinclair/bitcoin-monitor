c33cbdd834174a6838e38b8fff0571a4810ae5c4
Will Sinclair <will@willsinclair.ca>
2017-06-05 16:56:12 -0400

    configuring highcharts

--- app/assets/js/coin-monitor.js
+++ app/assets/js/coin-monitor.js
@@ -6,129 +6,171 @@ const request = require('request');
 const $ = require('jquery');
 const fs = require('fs');
 const os = require('os');
+const Highcharts = require('highcharts');
+require('highcharts/modules/exporting')(Highcharts);
 
 class CoinMonitor {
-  constructor() {
-    this.settingsDir = os.homedir() + "/.bitcoin-monitor";
-    this.settingsPath = this.settingsDir + "/config.json";
-    this.init();
-  }
+	constructor() {
+		this.settingsDir = os.homedir() + "/.bitcoin-monitor";
+		this.settingsPath = this.settingsDir + "/config.json";
+		this.init();
+	}
 
-  init() {
-    this.refreshSettings();
-    this.getUSDtoCAD();
-    this.getSpotPrice();
+	init() {
+		this.refreshSettings();
+		this.getUSDtoCAD();
+		this.getSpotPrice();
+		this.getPriceHistory();
 
-    setInterval(() => {
-      this.getSpotPrice();
-    }, this.settings.refreshrate);
-  }
-
-
-  getUSDtoCAD() {
-    // cex.io doesn't provide proce data in CAD, so I have to convert
-    request({
-      url: 'http://free.currencyconverterapi.com/api/v3/convert?q=USD_CAD'
-    }, (error, response, body) => {
-      if (error) console.log(error);
-      var data = JSON.parse(body).results.USD_CAD.val;
-      console.log(data);
-      this.usd_cad = data;
-    });
-  }
+		setInterval(() => {
+			this.getSpotPrice();
+		}, this.settings.refreshrate);
+	}
 
 
-  getPriceHistory() {
+	getUSDtoCAD() {
+		// cex.io doesn't provide proce data in CAD, so I have to convert
+		request({
+			url: 'http://free.currencyconverterapi.com/api/v3/convert?q=USD_CAD'
+		}, (error, response, body) => {
+			if (error) console.log(error);
+			var data = JSON.parse(body).results.USD_CAD.val;
+			this.usd_cad = data;
+		});
+	}
 
-  }
 
-  getSpotPrice() {
-    var symbol = "$";
-    if (this.settings.targetCurrency == "EUR") {
-      symbol = "€"
-    } else if (this.settings.targetCurrency == "GBP") {
-      symbol = "£"
-    }
-    var url = "https://cex.io/api/ticker/" + this.settings.baseCurrency + "/";
-    if (this.settings.targetCurrency == "CAD") {
-      url += "USD";
-    } else {
-      url += this.settings.targetCurrency;
-    }
-    request({
-        url: url
-      },
-      (error, response, body) => {
-        if (error) console.log(error);
-        var data = JSON.parse(body);
-        console.log(data);
-        var price = (Math.round(data.last * 100) / 100).toFixed(2);
-        var volume = (Math.round(data.volume * 100) / 100).toFixed(2);
-        if (this.settings.targetCurrency == "CAD") {
-          price = (Math.round(price * 100 * this.usd_cad) / 100).toFixed(2);
-          volume = (Math.round(volume * 100 * this.usd_cad) / 100).toFixed(2);
-        }
-        $("#current-price-display").text(symbol + price + " " + this.settings.targetCurrency);
-        $("#daily-volume-display").text("Volume: " + symbol + volume);
-      });
-  }
+	getPriceHistory() {
+		request.post('https://cex.io/api/price_stats/BTC/USD', {
+			form: {
+				"lastHours": 24,
+				"maxRespArrSize": 100
+			}
+		}, (error, response, body) => {
+			if (error) console.log(error);
+			var data = JSON.parse(body);
+			var options = {
+				chart: {
+					type: 'line',
+					marginRight: 30,
+					marginBottom: 30,
+					reflow: true
+				},
+				title: {
+					text: 'BTC price'
+				},
+				xAxis: {
+					type: 'datetime'
+				},
+				tooltip: {
+					valueDecimals: 2
+				},
+				legend: {
+					enabled: false
+				},
+				exporting: {
+					enabled: false
+				},
+				series: [{
+					name: 'BTC Price',
+					data: []
+				}]
+			};
 
-  refreshSettings() {
-    // create the app's home folder if it doesn't exist
-    if (!fs.existsSync(this.settingsDir)) {
-      fs.mkdirSync(this.settingsDir);
-    }
-    // if the config file exists
-    if (fs.existsSync(this.settingsPath) && this.settings == null) {
-      // read it into the object
-      this.settings = JSON.parse(fs.readFileSync(this.settingsPath, 'utf8'));
-    }
-    // if no config file exists
-    else {
-      // write one with default settings
-      var defaultSettings = {
-        baseCurrency: "BTC",
-        targetCurrency: "USD",
-        symbol: "$",
-        refreshrate: "5000"
-      };
-      this.settings = defaultSettings;
-      fs.writeFile(this.settingsPath, JSON.stringify(defaultSettings), {
-        encoding: 'utf8'
-      });
-    }
-    this.setUiFromSettings();
-  }
+			this.chart = Highcharts.chart('mid-bar-highcharts-graph', options);
+			for (var i = 0; i < data.length; i++) {
+				var item = data[i];
+				this.chart.series[0].addPoint([item.tmsp * 1000, Number(item.price)]);
+			}
+		});
+	}
 
-  updateSetting(name, value, callback) {
-    var tempSettings = this.settings;
-    tempSettings[name] = value;
-    this.settings = tempSettings;
-    this.changeUiFromSetting(name, value);
-    fs.writeFile(this.settingsPath, JSON.stringify(tempSettings), {
-      encoding: 'utf8'
-    }, function(err) { /* literally do nothing*/ });
-  }
+	getSpotPrice() {
+		var symbol = "$";
+		if (this.settings.targetCurrency == "EUR") {
+			symbol = "€"
+		} else if (this.settings.targetCurrency == "GBP") {
+			symbol = "£"
+		}
+		var url = "https://cex.io/api/ticker/" + this.settings.baseCurrency + "/";
+		if (this.settings.targetCurrency == "CAD") {
+			url += "USD";
+		} else {
+			url += this.settings.targetCurrency;
+		}
+		request({
+				url: url
+			},
+			(error, response, body) => {
+				if (error) console.log(error);
+				var data = JSON.parse(body);
+				var price = (Math.round(data.last * 100) / 100).toFixed(2);
+				var volume = (Math.round(data.volume * 100) / 100).toFixed(2);
+				if (this.settings.targetCurrency == "CAD") {
+					price = (Math.round(price * 100 * this.usd_cad) / 100).toFixed(2);
+					volume = (Math.round(volume * 100 * this.usd_cad) / 100).toFixed(2);
+				}
+				$("#current-price-display").text(symbol + price + " " + this.settings.targetCurrency);
+				$("#daily-volume-display").text("Volume: " + symbol + volume);
+			});
+	}
 
-  changeUiFromSetting(name, value) {
-    switch (name) {
-      case 'baseCurrency':
-        $("#base-currency-header").text(value);
-        $("#base-currency-display").text(value);
-        break;
-      case 'targetCurrency':
-        $("#target-currency-display").text(value);
-        break;
-    }
-  }
+	refreshSettings() {
+		// create the app's home folder if it doesn't exist
+		if (!fs.existsSync(this.settingsDir)) {
+			fs.mkdirSync(this.settingsDir);
+		}
+		// if the config file exists
+		if (fs.existsSync(this.settingsPath) && this.settings == null) {
+			// read it into the object
+			this.settings = JSON.parse(fs.readFileSync(this.settingsPath, 'utf8'));
+		}
+		// if no config file exists
+		else {
+			// write one with default settings
+			var defaultSettings = {
+				baseCurrency: "BTC",
+				targetCurrency: "USD",
+				symbol: "$",
+				refreshrate: "5000"
+			};
+			this.settings = defaultSettings;
+			fs.writeFile(this.settingsPath, JSON.stringify(defaultSettings), {
+				encoding: 'utf8'
+			});
+		}
+		this.setUiFromSettings();
+	}
 
-  setUiFromSettings() {
-    // base currency
-    $("#base-currency-header").text(this.settings.baseCurrency);
-    $("#base-currency-display").text(this.settings.baseCurrency);
-    // target currency
-    $("#target-currency-display").text(this.settings.targetCurrency);
-  }
+	updateSetting(name, value, callback) {
+		var tempSettings = this.settings;
+		tempSettings[name] = value;
+		this.settings = tempSettings;
+		this.changeUiFromSetting(name, value);
+		fs.writeFile(this.settingsPath, JSON.stringify(tempSettings), {
+			encoding: 'utf8'
+		}, function (err) { /* literally do nothing*/ });
+	}
+
+	changeUiFromSetting(name, value) {
+		switch (name) {
+			case 'baseCurrency':
+				$("#base-currency-header").text(value);
+				$("#base-currency-display").text(value);
+				break;
+			case 'targetCurrency':
+				$("#target-currency-display").text(value);
+				break;
+		}
+	}
+
+	setUiFromSettings() {
+		// base currency
+		$("#base-currency-header").text(this.settings.baseCurrency);
+		$("#base-currency-display").text(this.settings.baseCurrency);
+		// target currency
+		$("#target-currency-display").text(this.settings.targetCurrency);
+	}
 }
 
 module.exports = CoinMonitor;
--- app/assets/js/renderer.js
+++ app/assets/js/renderer.js
@@ -4,34 +4,47 @@ const CoinMonitor = require('./coin-monitor');
 const bootstrap = require('bootstrap');
 var monitor = new CoinMonitor();
 
-$(document).ready(function() {
-  function init() {
-    // minimize and close buttons
-    document.getElementById('minimize-button').addEventListener('click', (e) => {
-      var window = remote.getCurrentWindow();
-      window.minimize();
-    });
-    document.getElementById('close-button').addEventListener('click', (e) => {
-      var window = remote.getCurrentWindow();
-      window.close();
-    });
-  }
-  document.onreadystatechange = () => {
-    if (document.readyState == 'complete') {
-      init();
-    }
-  }
-  $("#base-currency-options").find("a").on('click', function(e) {
-    console.log($(this).text());
-    var newCurrency = $(this).text();
-    monitor.updateSetting('baseCurrency', newCurrency);
-    monitor.getSpotPrice();
-  });
+$(document).ready(function () {
+	var window = remote.getCurrentWindow();
 
-  $("#target-currency-options").find("a").on('click', function() {
-    console.log($(this).text());
-    var newCurrency = $(this).text();
-    monitor.updateSetting('targetCurrency', newCurrency);
-    monitor.getSpotPrice();
-  });
+
+	// minimize and close buttons
+	document.getElementById('minimize-button').addEventListener('click', (e) => {
+		window.minimize();
+	});
+	document.getElementById('close-button').addEventListener('click', (e) => {
+		window.close();
+	});
+
+
+	// change chart size on window size change
+	window.on('resize', () => {
+		var size = window.getContentSize();
+		monitor.chart.setSize(size[0] - 20, size[1] - 20, true);
+	});
+
+	// dropdown currency selectors
+	$("#base-currency-options").find("a").on('click', function (e) {
+		console.log($(this).text());
+		var newCurrency = $(this).text();
+		monitor.updateSetting('baseCurrency', newCurrency);
+		monitor.getSpotPrice();
+	});
+
+	$("#target-currency-options").find("a").on('click', function () {
+		console.log($(this).text());
+		var newCurrency = $(this).text();
+		monitor.updateSetting('targetCurrency', newCurrency);
+		monitor.getSpotPrice();
+	});
+
+
+	// chart toggle button
+	$("#show-price-chart-btn").on('click', () => {
+		$("#mid-bar-price-content").hide();
+		$("#highcharts-outer-container").show();
+		$("#mid-bar-highcharts-graph").show() //.css('visibility', 'visible');
+		var windowSize = (window.getContentSize());
+		monitor.chart.setSize(windowSize[0] - 20, windowSize[1] - 20, true);
+	});
 });
