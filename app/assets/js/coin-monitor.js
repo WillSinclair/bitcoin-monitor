69d25b4f4fee6c141444132a913b2bfa90bcc11e
William <w.sinclair@mail.utoronto.ca>
2017-06-05 09:13:34 -0400

    switched APIs

--- app/assets/js/coin-monitor.js
+++ app/assets/js/coin-monitor.js
@@ -8,89 +8,127 @@ const fs = require('fs');
 const os = require('os');
 
 class CoinMonitor {
-	constructor() {
-		this.settingsDir = os.homedir() + "/.bitcoin-monitor";
-		this.settingsPath = this.settingsDir + "/config.json";
-		this.refreshSettings();
-		this.getSpotPrice();
+  constructor() {
+    this.settingsDir = os.homedir() + "/.bitcoin-monitor";
+    this.settingsPath = this.settingsDir + "/config.json";
+    this.init();
+  }
 
-		setInterval(() => {
-			this.getSpotPrice();
-		}, this.settings.refreshrate);
-	}
+  init() {
+    this.refreshSettings();
+    this.getUSDtoCAD();
+    this.getSpotPrice();
 
-	getSpotPrice() {
-		var base = this.settings.baseCurrency.toLowerCase();
-		var target = this.settings.targetCurrency.toLowerCase();
-		request({
-				url: "https://api.cryptonator.com/api/ticker/" + base + "-" + target
-			},
-			(error, response, body) => {
-				if (error) console.log('oops');
-				var data = JSON.parse(body).ticker;
-				console.log(data);
-				data.price = Math.round(data.price * 100) / 100;
-				$("#current-price-display").text("$" + data.price + " " + data.target);
-			});
-	}
+    setInterval(() => {
+      this.getSpotPrice();
+    }, this.settings.refreshrate);
+  }
 
-	refreshSettings() {
-		// create the app's home folder if it doesn't exist
-		if (!fs.existsSync(this.settingsDir)) {
-			fs.mkdirSync(this.settingsDir);
-		}
-		// if the config file exists
-		if (fs.existsSync(this.settingsPath)) {
-			// read it into the object
-			this.settings = JSON.parse(fs.readFileSync(this.settingsPath, 'utf8'));
-		}
-		// if no config file exists
-		else {
-			// write one with default settings
-			var defaultSettings = {
-				baseCurrency: "BTC",
-				targetCurrency: "CAD",
-				refreshrate: "3000"
-			};
-			this.settings = defaultSettings;
-			fs.writeFile(this.settingsPath, JSON.stringify(defaultSettings), {
-				encoding: 'utf8'
-			});
-		}
-		this.setUiFromSettings();
-	}
 
-	updateSetting(name, value) {
-		try {
-			var tempSettings = this.settings;
-			tempSettings.name = value;
-			this.settings = tempSettings;
-			this.changeUiFromSetting(name, value);
-			fs.writeFile(this.settingsPath, JSON.stringify(tempSettings), {
-				encoding: 'utf8'
-			});
-		} catch (Error) {
-			console.log('Failed to set setting' + name + ' to ' + value);
-		}
-	}
+  getUSDtoCAD() {
+    // cex.io doesn't provide proce data in CAD, so I have to convert
+    request({
+      url: 'http://free.currencyconverterapi.com/api/v3/convert?q=USD_CAD'
+    }, (error, response, body) => {
+      if (error) console.log(error);
+      var data = JSON.parse(body).results.USD_CAD.val;
+      console.log(data);
+      this.usd_cad = data;
+    });
+  }
 
-	changeUiFromSetting(name, value) {
-		switch (name) {
-			case 'baseCurrency':
-				$("#base-currency-display").text(value);
-				break;
-			case 'targetCurrency':
-				$("#target-currency-display").text(value);
-				break;
-		}
-	}
 
-	setUiFromSettings() {
-		// base currency
-		$("#base-currency-display").text(this.settings.baseCurrency);
-		// target currency
-		$("#target-currency-display").text(this.settings.targetCurrency);
-	}
+  getPriceHistory() {
+
+  }
+
+  getSpotPrice() {
+    var symbol = "$";
+    if (this.settings.targetCurrency == "EUR") {
+      symbol = "€"
+    } else if (this.settings.targetCurrency == "GBP") {
+      symbol = "£"
+    }
+    var url = "https://cex.io/api/ticker/" + this.settings.baseCurrency + "/";
+    if (this.settings.targetCurrency == "CAD") {
+      url += "USD";
+    } else {
+      url += this.settings.targetCurrency;
+    }
+    request({
+        url: url
+      },
+      (error, response, body) => {
+        if (error) console.log(error);
+        var data = JSON.parse(body);
+        console.log(data);
+        var price = (Math.round(data.last * 100) / 100).toFixed(2);
+        var volume = (Math.round(data.volume * 100) / 100).toFixed(2);
+        if (this.settings.targetCurrency == "CAD") {
+          price = (Math.round(price * 100 * this.usd_cad) / 100).toFixed(2);
+          volume = (Math.round(volume * 100 * this.usd_cad) / 100).toFixed(2);
+        }
+        $("#current-price-display").text(symbol + price + " " + this.settings.targetCurrency);
+        $("#daily-volume-display").text("Volume: " + symbol + volume);
+      });
+  }
+
+  refreshSettings() {
+    // create the app's home folder if it doesn't exist
+    if (!fs.existsSync(this.settingsDir)) {
+      fs.mkdirSync(this.settingsDir);
+    }
+    // if the config file exists
+    if (fs.existsSync(this.settingsPath) && this.settings == null) {
+      // read it into the object
+      this.settings = JSON.parse(fs.readFileSync(this.settingsPath, 'utf8'));
+    }
+    // if no config file exists
+    else {
+      // write one with default settings
+      var defaultSettings = {
+        baseCurrency: "BTC",
+        targetCurrency: "USD",
+        symbol: "$",
+        refreshrate: "5000"
+      };
+      this.settings = defaultSettings;
+      fs.writeFile(this.settingsPath, JSON.stringify(defaultSettings), {
+        encoding: 'utf8'
+      });
+    }
+    this.setUiFromSettings();
+  }
+
+  updateSetting(name, value, callback) {
+    var tempSettings = this.settings;
+    tempSettings[name] = value;
+    this.settings = tempSettings;
+    this.changeUiFromSetting(name, value);
+    fs.writeFile(this.settingsPath, JSON.stringify(tempSettings), {
+      encoding: 'utf8'
+    }, function(err) { /* literally do nothing*/ });
+  }
+
+  changeUiFromSetting(name, value) {
+    switch (name) {
+      case 'baseCurrency':
+        $("#base-currency-header").text(value);
+        $("#base-currency-display").text(value);
+        break;
+      case 'targetCurrency':
+        $("#target-currency-display").text(value);
+        break;
+    }
+  }
+
+  setUiFromSettings() {
+    // base currency
+    $("#base-currency-header").text(this.settings.baseCurrency);
+    $("#base-currency-display").text(this.settings.baseCurrency);
+    // target currency
+    $("#target-currency-display").text(this.settings.targetCurrency);
+  }
 }
 
 module.exports = CoinMonitor;
--- app/assets/js/renderer.js
+++ app/assets/js/renderer.js
@@ -4,21 +4,34 @@ const CoinMonitor = require('./coin-monitor');
 const bootstrap = require('bootstrap');
 var monitor = new CoinMonitor();
 
-(function () {
-	function init() {
-		// minimize and close buttons
-		document.getElementById('minimize-button').addEventListener('click', (e) => {
-			var window = remote.getCurrentWindow();
-			window.minimize();
-		});
-		document.getElementById('close-button').addEventListener('click', (e) => {
-			var window = remote.getCurrentWindow();
-			window.close();
-		});
-	}
-	document.onreadystatechange = () => {
-		if (document.readyState == 'complete') {
-			init();
-		}
-	}
-})();
+$(document).ready(function() {
+  function init() {
+    // minimize and close buttons
+    document.getElementById('minimize-button').addEventListener('click', (e) => {
+      var window = remote.getCurrentWindow();
+      window.minimize();
+    });
+    document.getElementById('close-button').addEventListener('click', (e) => {
+      var window = remote.getCurrentWindow();
+      window.close();
+    });
+  }
+  document.onreadystatechange = () => {
+    if (document.readyState == 'complete') {
+      init();
+    }
+  }
+  $("#base-currency-options").find("a").on('click', function(e) {
+    console.log($(this).text());
+    var newCurrency = $(this).text();
+    monitor.updateSetting('baseCurrency', newCurrency);
+    monitor.getSpotPrice();
+  });
+
+  $("#target-currency-options").find("a").on('click', function() {
+    console.log($(this).text());
+    var newCurrency = $(this).text();
+    monitor.updateSetting('targetCurrency', newCurrency);
+    monitor.getSpotPrice();
+  });
+});
