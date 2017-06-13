f7a13798d60a63649e178a242459d2e740ac13b5
Will Sinclair <will@willsinclair.ca>
2017-06-13 16:30:38 -0400

    store window size/position

--- main.js
+++ main.js
@@ -1,30 +1,74 @@
 const {
 	app,
-	BrowserWindow
+	BrowserWindow,
+	Menu,
+	MenuItem
 } = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
+const os = require('os');
+const menu = new Menu();
 let win;
 
-
-// global shared object
-global.settings = {
-	spotPrice: null,
-	buyPrice: null,
-	sellPrice: null,
-	currency: null
+const settingsDir = os.homedir() + "/.bitcoin-monitor";
+const settingsPath = settingsDir + "/window-config.json";
+const defaultSettings = {
+	title: 'Crypto Monitor',
+	backgroundColor: '#222',
+	width: 600,
+	height: 300,
+	frame: false,
+	webPreferences: {
+		nodeIntegrationInWorker: true
+	},
+	show: false // hide the window until ready
 };
+var settings = readWindowSettings();
+
+
+function readWindowSettings() {
+	// create the app's home folder if it doesn't exist
+	if (!fs.existsSync(settingsDir)) {
+		fs.mkdirSync(settingsDir);
+	}
+	// if the config file exists
+	if (fs.existsSync(settingsPath)) {
+		// read it into the object
+		settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
+	}
+	// if no config file exists
+	else {
+		settings = defaultSettings;
+		writeWindowSettings(settings);
+	}
+	return settings;
+}
+
+
+function writeWindowSettings() {
+	// write one with default settings
+	fs.writeFile(settingsPath, JSON.stringify(settings), {
+		encoding: 'utf8'
+	}, (err) => {
+		// if the file fails to write... oh well it's just a window config
+		console.log(err);
+	}, () => {});
+}
 
 function createWindow() {
-	// Create the browser window.
-	win = new BrowserWindow({
-		width: 600,
-		height: 300,
-		frame: false,
-		webPreferences: {
-			nodeIntegrationInWorker: true
-		}
-	});
+	try {
+		// Create the browser window.
+		win = new BrowserWindow(settings);
+	} catch (err) {
+		win = new BrowserWindow(defaultSettings);
+	}
+
+	// show the window when it's ready
+	win.on('ready-to-show', () => {
+		win.show();
+		win.focus();
+	})
 
 	// and load the index.html of the app.
 	win.loadURL(url.format({
@@ -36,12 +80,20 @@ function createWindow() {
 	// Open the DevTools.
 	win.webContents.openDevTools();
 
-	// Emitted when the window is closed.
 	win.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		win = null;
+		writeWindowSettings(settings);
+		win = null; // dereference the closed window
+	});
+
+	win.on('move', () => {
+		var position = win.getPosition();
+		settings.x = position[0];
+		settings.y = position[1];
+	});
+	win.on('resize', () => {
+		var size = win.getSize();
+		settings.width = size[0];
+		settings.height = size[1];
 	});
 }
 
@@ -55,6 +107,7 @@ app.on('window-all-closed', () => {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
+		writeWindowSettings();
 		app.quit();
 	}
 });
